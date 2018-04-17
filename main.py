60581bb501390d29464398dacdc972cefa16790e
arturoverbel <Universidad#Github>
2018-04-17 10:42:20 -0500

    Dikstra ready!

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import graph_pro as g
 import os
 
 os.system('cls')
+print("<------------------------>")
 print("Main\n")
 
-source = [1,2,2,2,3,4,2]
-target = [2,1,3,4,4,3,5]
-weight = [1,1,2,4,1,1,1]
+source = [1,2,2,2,3,4,2,5]
+target = [2,1,3,4,4,3,5,4]
+weight = [1,1,2,4,1,1,2,2]
 
 G = g.graph_pro(source, target, weight)
-print(G.print())
+G.print()
+
+print("------------------------")
+print("Dikstra: ")
+print(G.dijkstra(1))
