971c9baefef818d60695891cd601b3a2deccf55d
Arturo Verbel <arturo-verbel@hotmail.com>
2018-10-24 21:44:56 -0500

    Algoritm Khopkar

--- test_incremental_khopkar.py
+++ test_incremental_khopkar.py
@@ -25,3 +25,24 @@ print(dist_new)
 
 graph.print_r()
 #graph.draw()
+
+
+sources = [10, 11, 5, 5, 6, 4, 8, 4, 9, 9,  2, 12, 3]
+targets = [1,   1, 1, 6, 4, 7, 4, 9, 8, 2, 12,  9, 2]
+weights = [1,   1, 1, 1, 1, 1, 1, 1, 1, 1,  1,  1, 1]
+graph2 = g(sources, targets, weights)
+
+print('.........................')
+
+dist2 = graph2.floyd_warshall()
+print(dist2)
+
+graph2.dynamic_incremental_node(
+    node=13, sources=[1, 3], w_sources=[1, 1], targets=[2, 4], w_targets=[1, 1])
+
+dist2_new = graph2.kk_incremental(dist2.tolist())
+print(dist2_new)
+
+graph2.print_r()
+
+
